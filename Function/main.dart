9c76d8a492b8fa2de5fd76ffb993e5066a1b888e
aldyisza87 <57266292+aldyisza87@users.noreply.github.com>
2023-05-07 22:58:35 +0700

    latihan Function

---
+++ Function/main.dart
@@ -0,0 +1,9 @@
+String gantiKapital(String input) {
+  return input.toUpperCase();
+}
+
+void main() {
+  String intput = "semangat belajar dart flutter";
+  String output = gantiKapital(intput);
+  print(output);
+}
