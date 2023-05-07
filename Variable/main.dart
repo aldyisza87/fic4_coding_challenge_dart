bd2f59a544ada8ec0c501a55163f2d3d4faff793
aldyisza87 <57266292+aldyisza87@users.noreply.github.com>
2023-05-07 16:15:30 +0700

    latihan variable

---
+++ Variable/main.dart
@@ -0,0 +1,8 @@
+void main() {
+  double panjang = 12;
+  double lebar = 10;
+  double luas = panjang * lebar;
+
+  print(
+      "hasil dari luas persegi panjang dengan Panjang : $panjang dan Lebar : $lebar Adalah = $luas");
+}
